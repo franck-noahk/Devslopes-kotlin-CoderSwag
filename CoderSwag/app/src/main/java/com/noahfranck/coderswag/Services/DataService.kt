aaaf30b82421269398962c83203f6ee74d4a9dca
Noah F <franck.noahk@gmail.com>
2019-05-24 16:52:27 -0500

    Added the list of Categories

--- CoderSwag/app/src/main/java/com/noahfranck/coderswag/Services/DataService.kt
+++ CoderSwag/app/src/main/java/com/noahfranck/coderswag/Services/DataService.kt
@@ -1,4 +1,13 @@
 package com.noahfranck.coderswag.Services
 
+import com.noahfranck.coderswag.Model.Category
+
 object DataService {
+    val vategories = listOf(
+        Category("SHIRTS","shirtimage"),
+        Category("HOODIES", "hoodieimage"),
+        Category("HATS","hatimage"),
+        Category("DIGITAL","digitalgoodimage")
+    )
+
 }
