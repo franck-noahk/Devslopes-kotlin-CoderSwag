c6c2ad14d4003b7eb0e55be640aeaa34c2dde1c4
Noah F <franck.noahk@gmail.com>
2019-06-01 18:17:55 -0500

    Set up bare bones for Recycler View.

---
+++ CoderSwag/app/src/main/java/com/noahfranck/coderswag/Adapter/CategoryRecyclerAdapter.kt
@@ -0,0 +1,23 @@
+package com.noahfranck.coderswag.Adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class CategoryRecyclerAdapter : RecyclerView.Adapter<CategoryRecyclerAdapter.holder> () {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): holder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: holder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
