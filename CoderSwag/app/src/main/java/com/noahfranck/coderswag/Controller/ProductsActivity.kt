c970665feee273478af775b4964c98002cefb395
Noah F <franck.noahk@gmail.com>
2019-06-01 22:47:54 -0500

    Created new Activity

---
+++ CoderSwag/app/src/main/java/com/noahfranck/coderswag/Controller/ProductsActivity.kt
@@ -0,0 +1,13 @@
+package com.noahfranck.coderswag.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.noahfranck.coderswag.R
+
+class ProductsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_products)
+    }
+}
