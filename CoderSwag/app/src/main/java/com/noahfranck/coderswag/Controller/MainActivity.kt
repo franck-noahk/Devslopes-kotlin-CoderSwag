f24a277598228e0bad5156ec633879aacbde828d
Noah F <franck.noahk@gmail.com>
2019-06-01 23:01:23 -0500

    Passing the title through intent

--- CoderSwag/app/src/main/java/com/noahfranck/coderswag/Controller/MainActivity.kt
+++ CoderSwag/app/src/main/java/com/noahfranck/coderswag/Controller/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.noahfranck.coderswag.Controller
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import com.noahfranck.coderswag.Adapter.CategoryRecyclerAdapter
 import com.noahfranck.coderswag.R
 import com.noahfranck.coderswag.Services.DataService
+import com.noahfranck.coderswag.Utilities.EXTRA_CATAGORY
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +19,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         adapter = CategoryRecyclerAdapter(DataService.categories, this){ category ->
-
+            val productIntent = Intent(this,ProductsActivity::class.java)
+            productIntent.putExtra(EXTRA_CATAGORY, category.title)
+            startActivity(productIntent)
         }
 
         categoryListView.adapter = adapter
--- CoderSwag/app/src/main/java/com/noahfranck/coderswag/Controller/ProductsActivity.kt
+++ CoderSwag/app/src/main/java/com/noahfranck/coderswag/Controller/ProductsActivity.kt
@@ -3,11 +3,13 @@ package com.noahfranck.coderswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.noahfranck.coderswag.R
+import com.noahfranck.coderswag.Utilities.EXTRA_CATAGORY
 
 class ProductsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
+        val categoryType = intent.getStringExtra(EXTRA_CATAGORY)
     }
 }
