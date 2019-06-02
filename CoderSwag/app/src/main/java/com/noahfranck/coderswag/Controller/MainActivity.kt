9581dd911272d03c35f670198a1d3f67fc0f61c6
Noah F <franck.noahk@gmail.com>
2019-06-01 22:25:39 -0500

    Switched the controller to Recycler adapter

--- CoderSwag/app/src/main/java/com/noahfranck/coderswag/Controller/MainActivity.kt
+++ CoderSwag/app/src/main/java/com/noahfranck/coderswag/Controller/MainActivity.kt
@@ -2,29 +2,31 @@ package com.noahfranck.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Toast
-import com.noahfranck.coderswag.Adapter.CategoryAdapter
-import com.noahfranck.coderswag.Model.Category
+import android.support.v7.widget.LinearLayoutManager
+import com.noahfranck.coderswag.Adapter.CategoryRecyclerAdapter
 import com.noahfranck.coderswag.R
 import com.noahfranck.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: CategoryAdapter
+    lateinit var adapter: CategoryRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this,
-            DataService.categories
+        adapter = CategoryRecyclerAdapter(
+            DataService.categories,
+            this
         )
 
         categoryListView.adapter = adapter
+        //old implementation for previous adapter
 
-        
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
 
     }
 }
