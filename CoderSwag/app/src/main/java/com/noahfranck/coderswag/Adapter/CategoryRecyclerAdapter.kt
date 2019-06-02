b053d40135d95fa3db14b1a19bf61a66d6b73c94
Noah F <franck.noahk@gmail.com>
2019-06-01 22:26:29 -0500

    Bulk of recycler details
    This is mostly everything form switching to Recycler View.

--- CoderSwag/app/src/main/java/com/noahfranck/coderswag/Adapter/CategoryRecyclerAdapter.kt
+++ CoderSwag/app/src/main/java/com/noahfranck/coderswag/Adapter/CategoryRecyclerAdapter.kt
@@ -1,23 +1,43 @@
 package com.noahfranck.coderswag.Adapter
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.noahfranck.coderswag.Model.Category
+import com.noahfranck.coderswag.R
 
-class CategoryRecyclerAdapter : RecyclerView.Adapter<CategoryRecyclerAdapter.holder> () {
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): holder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+class CategoryRecyclerAdapter(val categories: List<Category>, val context:Context) : RecyclerView.Adapter<CategoryRecyclerAdapter.Holder> () {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent?.context).inflate(R.layout.category_list_item, parent, false)
+        return Holder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return categories.count()
     }
 
-    override fun onBindViewHolder(p0: holder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder?.bindCategory(categories[position], context)
+
     }
 
-    inner class holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val categoryImage = itemView?.findViewById<ImageView>(R.id.categoryImage)
+        val categoryName = itemView?.findViewById<TextView>(R.id.catageoryName)
+
+        fun bindCategory(category: Category, context: Context){
+            val resourceId = context.resources.getIdentifier(category.image,"drawable", context.packageName)
+            categoryImage?.setImageResource(resourceId)
+            categoryName?.text = category.title
+
+        }
 
     }
 }
